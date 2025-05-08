0ff2c8953ce10cbc6ed85267ff69a924cabd4e46
Miroslav Horvatov <52826419+kimleta@users.noreply.github.com>
2025-05-08 15:34:51 +0200

    Structure change

--- src/DlhGoogleMapsBundle.php
+++ src/DlhGoogleMapsBundle.php
@@ -1,9 +1,16 @@
 <?php
 
 namespace delahaye\googlemaps;
-
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+
 
 class DlhGoogleMapsBundle extends Bundle
 {
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+
+
+
 }
